cbb98699308d2a6b9c6e3615c23520cfb2910d04
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2021-12-18 16:35:54 +0100

    usunięcie zbędnych komentarzy w gauss.c

--- src/gauss.c
+++ src/gauss.c
@@ -1,13 +1,13 @@
 #include "gauss.h"
-#include <stdio.h>   //dodane zeby wypisac blad przez printf
+#include <stdio.h>   // dla printf
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate(Matrix *mat, Matrix *b){  		 // *mat to dpowiednik *A
+int eliminate(Matrix *mat, Matrix *b){  		
 	
-	int n = mat->r;                                  // k to liczba wierszy macierzy 
+	int n = mat->r;                                
 
 	for( int k= 0; k < n-1; k++ ) {
 		for( int w= k+1; w < n; w++ ) {
@@ -21,7 +21,7 @@ int eliminate(Matrix *mat, Matrix *b){  		 // *mat to dpowiednik *A
 			b->data[w][0] -= q*b->data[k][0] ;
 		}
 	}		    
-						  	 // Tutaj należy umieścić właściwą implemnentację
+						  	 
 	return 0;
 }
 
