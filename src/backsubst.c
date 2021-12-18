5cd03cb53eb514f27cb302dca2bb7a66fcab5fa6
sebastianp265 <pawlinskisebastian@gmail.com>
2021-12-18 14:51:07 +0100

    implementacja pliku backsubst.c

--- src/backsubst.c
+++ src/backsubst.c
@@ -4,19 +4,24 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int backsubst(Matrix *x, Matrix *mat, Matrix *b)
+{
+    int n = mat->r;
+    int rb = b->r;
+    int rx = x->r;
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+    if (rb != n || rb != rx) //sprawdzenie poprawności rozmiarów macierzy
+        return 2;
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+    for (int w = n - 1; w >= 0; w--)
+    {
+        double s = 0;
+        for (int k = w + 1; k < n; k++)
+            s += (mat->data[w][k]) * (x->data[k][0]);
+        if ((mat->data[w][w]) == 0) // element na diagonali równy 0
+            return 1;
+        x->data[w][0] = (b->data[w][0] - s) / (mat->data[w][w]);
+    }
 
-				return 0;
+    return 0;
 }
-
-
