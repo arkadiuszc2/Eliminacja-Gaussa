0d049bd999883ceae1cc4ba57e72e2ac7f546a99
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2021-12-21 22:14:24 +0100

    dodanie wyboru elementu maksymalnego w funkcji eliminate  w gauss.c

--- src/gauss.c
+++ src/gauss.c
@@ -10,6 +10,18 @@ int eliminate(Matrix *mat, Matrix *b){
 	int n = mat->r;                                
 
 	for( int k= 0; k < n-1; k++ ) {
+		int max = k;				// wybór elementu dominującego w kolumnie i zamiana równań miejscami
+		for ( int w=k+1; w < n; w++)
+			if( mat->data[w][k] > mat->data[mx][k])
+				mx = w;
+		if( mx != k){				// zamiana wierszy jeśli mx nie jest równe k
+			double *tmp = mat->data[k];
+			mat->data[k] = mat->data[mx];
+			mat->data[mx] = tmp;
+			double *tr = b->data[k];
+			b->data[k] = b->data[mx];
+			b->data[mx] = tr;
+		}			
 		for( int w= k+1; w < n; w++ ) {
 			if((mat->data[k][k]) == 0) {
 				printf("Błąd, dzielenie przez 0\n");
